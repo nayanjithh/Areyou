463e6135b4557fd3f862d1d439a442731b4c865b
NAYANJITH <nayanjithajith@gmail.com>
2024-07-30 22:40:29 +0530

    overflow fixed

--- Idiot/idiot.js
+++ Idiot/idiot.js
@@ -2,11 +2,16 @@ function change(){
             let a=Math.random()*1000;
             let b=Math.random()*1000;
             const value=document.querySelector('.no');
+            const body = document.querySelector('body');
             value.style.position="absolute";
-            if(a>870)
-                value.style.right=870+"px";
-            else
+            body.style.position="relative";
+            if(a>870 || b>270 ){
+                value.style.right=a/10+"%";
+                value.style.top=b/10+"%";
+            }
+            else{
                 value.style.right=a+'px';
-            value.style.top=b+'px';
+                value.style.top=b+'px';
+            }
             document.querySelector('.try').innerHTML="Try again!";
         }
